1dabedd01cae0d1fdad85a0326a80f17b6bb3b1c
TarunBits <tarunbhattmortal@gmail.com>
2025-11-06 17:35:45 +0530

    String and Boolean Data Types

---
+++ string.js
@@ -0,0 +1,25 @@
+//? Ways to define string
+var str = "Hello How are you";
+var str2 = "Hello How are you";
+var str3 = "Hello How are you";
+var age = 20;
+var userName = "Anil sidhu";
+//? Convert in string data type
+var info = "my name is ".concat(userName, " and age is ").concat(age);
+console.log(info);
+var num = 1;
+// var data: string = num.toString();
+// var data: string = " " + num;
+var boodData = true;
+// var data: string = boodData.toString();
+var data = "" + boodData;
+//? Apply boolean values
+var item = true;
+//? Possible boolean values
+var other;
+// other = "hello"
+other = item;
+console.log(other);
+var info1 = "anil siddhu";
+//? Inference and declaration issues
+var str10 = "hey this is anil";
