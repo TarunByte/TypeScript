5d674db59580605fd3603ae5c1497051ecadeb6b
TarunBits <tarunbhattmortal@gmail.com>
2025-11-08 20:17:19 +0530

    Never Data Type in TypeScript

---
+++ output/never.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function loopFunction() {
+    while (true) {
+        console.log("loop");
+    }
+}
+function simple() {
+    throw new Error("output and data not found");
+}
